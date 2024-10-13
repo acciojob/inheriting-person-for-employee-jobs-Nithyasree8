a693cb403adf46eaaa390bb98bbb8c8a406459c5
Nithyasree8 <113105796+Nithyasree8@users.noreply.github.com>
2024-10-13 17:45:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 
     // Greet method
     this.greet = function() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
 }
 
@@ -25,6 +25,12 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
+// Create instances and call methods
+const person = new Person('Alice', 25);
+person.greet();
+
+const employee = new Employee('Bob', 30, 'Manager');
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
